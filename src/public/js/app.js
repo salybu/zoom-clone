6ead5ff5a5d4c667909a4d18aa2156a1d317f7af
salybu <rkdmsdl93@gmail.com>
2022-03-27 11:52:27 +0900

    feat: socket con/discon/ send message from B to F

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,16 @@
 // alert("hi");
 
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ⭕");
+});
+
+socket.addEventListener("message", (message) => {
+  //   console.log("Just got this: ", message, " from the server");
+  console.log("Just got this: ", message.data, " from the server");
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
